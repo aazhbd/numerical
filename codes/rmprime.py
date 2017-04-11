a40d2e071a7e1bf334e4da94b429425f6b880c8f
aazhbd <aazhbd@yahoo.com>
2017-04-11 11:05:40 +0200

    Added prime number test.

---
+++ codes/rmprime.py
@@ -0,0 +1,53 @@
+#!/home/zakir/Downloads/env/bin/python
+"""
+Generate large primes using Probabilistic tests
+"""
+import random
+import sys
+
+def is_probable_prime(n, k = 7):
+   """use Rabin-Miller algorithm to return True (n is probably prime)
+      or False (n is definitely composite)"""
+   if n < 6:  # assuming n >= 0 in all cases... shortcut small cases here
+      return [False, False, True, True, False, True][n]
+   elif n & 1 == 0:  # should be faster than n % 2
+      return False
+   else:
+      s, d = 0, n - 1
+      while d & 1 == 0:
+         s, d = s + 1, d >> 1
+      # Use random.randint(2, n-2) for very large numbers
+      for a in random.sample(xrange(2, min(n - 2, sys.maxint)), min(n - 4, k)):
+         x = pow(a, d, n)
+         if x != 1 and x + 1 != n:
+            for r in xrange(1, s):
+               x = pow(x, 2, n)
+               if x == 1:
+                  return False  # composite for sure
+               elif x == n - 1:
+                  a = 0  # so we know loop didn't continue to end
+                  break  # could be strong liar, try another a
+            if a:
+               return False  # composite if we reached end of this loop
+      return True  # probably prime if reached end of outer loop
+
+
+def main():
+    f = open('primes.txt', 'w')
+    i = 3
+    while True:
+        if is_probable_prime(i, 2):
+            f.write(str(i) + '\n')
+            print str(i) + ' is probably prime\n'
+        i += 2
+        if i > 99999999999999999999:
+            break
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+#9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999499999
